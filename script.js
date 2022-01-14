58edcb7e72a284f9ec8eb06b5ef37faf69f49165
gefgu <gefgu@hotmail.com>
2022-01-14 15:45:28 -0300

    Remove game function

--- script.js
+++ script.js
@@ -31,15 +31,3 @@ function playRound(playerSelection, computerSelection) {
         return `${playerSelection} is an invalid choice`;
     }
 }
-
-// Repeat 5 times the code below:
-// get the player input
-// get the computerPlay
-// display the result of playRound
-function game() {
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = prompt("You choose Rock, Paper or Scissors?");
-        let computerSelection = computerPlay();
-        console.log(playRound(playerSelection, computerSelection));
-    }
-}
